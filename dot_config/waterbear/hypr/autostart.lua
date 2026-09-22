0d04479a1ec584d86336c2d003bbf51fc01f93ef
Caleb Mohamed <calebnoahmohamed@gmail.com>
2026-09-22 22:00:10 +0100

    added a weather oracle and decided not to add it to the autostart

--- dot_config/waterbear/hypr/autostart.lua
+++ dot_config/waterbear/hypr/autostart.lua
@@ -18,4 +18,3 @@ hl.on("hyprland.start", function ()
   hl.exec_cmd("dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP=dwl")
   hl.exec_cmd("nm-applet --indicator")
 end)
-
